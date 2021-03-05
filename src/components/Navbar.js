f9afdfb54ce13935f86af7a0ffcbb22b15582c77
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-05 11:54:33 +0100

    7. sidebar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ toggle }) => {
           <Link to="/" className="nav-logo">
             <img src={logo} alt="mdx-logo" />
           </Link>
-          <button className="toggle-btn">
+          <button className="toggle-btn" onClick={toggle}>
             <FaBars />
           </button>
         </div>
