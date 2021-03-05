5c15b501d4fd439f8b0a2c06b955f3a616d52813
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-05 14:26:10 +0100

    12. collect form values in netlify

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -8,7 +8,18 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all the latest stories to your inbox</h2>
           <h4>I write to my friends every few weeks</h4>
-          <form action="" className="contact-form">
+          <form
+            action=""
+            className="contact-form"
+            className="contact-form"
+            name="testing-contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="testing-contact" />
             <input
               type="text"
               name="name"
