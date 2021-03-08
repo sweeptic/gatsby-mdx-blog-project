170d98799b5d963a39362eeac22845c244a1c789
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-08 11:59:37 +0100

    36. banner categories

--- src/components/Banner/BannerCategories.js
+++ src/components/Banner/BannerCategories.js
@@ -2,9 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import Categories from '../Categories'
 import Title from './Title'
+
 const BannerCategories = () => {
-  return <Wrapper>Banner Categories</Wrapper>
+  return (
+    <Wrapper>
+      <Title title="categories" />
+      <Categories />
+    </Wrapper>
+  )
 }
+
 const Wrapper = styled.div`
   .category {
     font-size: 1rem;
